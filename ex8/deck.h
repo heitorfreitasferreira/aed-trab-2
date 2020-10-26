06954ccaa381356af32596ec604388353406058c
heitorfreitasferreira <heitor.ff@ufu.br>
2020-10-26 01:44:46 -0300

    8 finalizada familia só o báisco

--- ex8/deck.h
+++
@@ -1,9 +0,0 @@
-typedef struct deque *Deque;
-//Metodos das estruturas
-Deque cria_Deque();
-int deque_vazio(Deque);
-int push(Deque, float);    //insert end
-int pop(Deque, float *);   //remove end
-int shift(Deque, float *); //remove beg
-int unshift(Deque, float); //insert beg
-int libera_deque(Deque *); //apaga tudo
--- ex8/main.c
+++ ex8/main.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> // SOMENTE PARA SISTEMAS SUPERIORES
-#include "deck.h"
+#include "deque.h"
 #define tam 3
 int main(int argc, char const *argv[])
 {
   int option = 1, continuar = 1, tmp = 0, tmp2 = 0, qnt_lista = 0;
-  char letra;
+  float elemento;
   Deque lst[tam];
   int lst_ocupados[tam] = {0};
   while (continuar)
   {
-    printf("1: Criar lista lst\n2: Insere na lista\n3: Remove primeira ocorrencia\n4: Remove segundo a posicao\n5: Imprimir lista\n6: Limpar uma lista\n7: Tamanho e uma lista\n8: Ver se as listas sao iguais\n9: Exibir maior elemento de uma lista\n10: Intercalar duas listas em uma terceira\n11: Sair\n\nOpcao: ");
+  get_back_here:
+    printf("1: Criar lista\n");
+    printf("2: Insere no comeco\n");
+    printf("3: Remove no comeco\n");
+    printf("4: Insere no final\n");
+    printf("5: Remove no final\n");
+    printf("6: Print\n");
+    printf("7: Apaga lista\n");
+    printf("8: Sair\n");
+    printf("\nOpcao: ");
     scanf("%d", &option);
     switch (option)
     {
     case 1:
-      /* CRIANDO LISTA */
+      /* CRIANDO DEQUE */
       if (qnt_lista < tam)
       {
         int i;
@@ -26,8 +35,8 @@ int main(int argc, char const *argv[])
             break;
         }
 
-        lst[i] = cria_lista();
-        printf("Lista %d criada com sucesso\n", i);
+        lst[i] = cria_deque();
+        printf("Lista %d criada com sucesso\n", i + 1);
         lst_ocupados[i]++;
         qnt_lista++;
       }
@@ -35,160 +44,154 @@ int main(int argc, char const *argv[])
       {
         printf("Quantidade maximas de listas criadas tente outro comando\n");
       }
-      sleep(2);
+      sleep(1);
       system("clear"); //MUDAR PRA CLS PRA SISTEMAS INFERIORES
       break;
 
     case 2:
-      /* INSERINDO NA LISTA */
+      /* INSERINDO NO COMECO */
+      if (qnt_lista == 0)
+        goto get_back_here;
 
-      printf("Em qual lista deseja inserir");
+      printf("Em qual comeco de lista deseja inserir");
       for (size_t i = 0; i < qnt_lista; i++)
-        printf(" %ld", i);
+        printf(" %ld", i + 1);
       printf(": ");
       scanf("%d", &tmp);
-      printf("Insira a letra que deseja inserir na lista %d: ", tmp);
+      printf("Insira o num que deseja inserir na lista %d: ", tmp);
+      tmp--;
       setbuf(stdin, NULL);
-      scanf("%c", &letra);
-      if (insere_ord(&lst[tmp], letra))
+      scanf("%f", &elemento);
+      if (unshift(lst[tmp], elemento))
       {
-        printf("Sucesso");
+        printf("Sucesso\n");
         sleep(0.3);
         system("clear"); //MUDAR PRA CLS PRA SISTEMAS INFERIORES
       }
       else
       {
         printf("Erro");
-        sleep(2);
+        sleep(1);
         system("clear"); //MUDAR PRA CLS PRA SISTEMAS INFERIORES
       }
 
       break;
 
     case 3:
-      printf("Em qual lista deseja remover");
+      /* REMOVENDO NO COMECO */
+      if (qnt_lista == 0)
+        goto get_back_here;
+      /* REMOVE NO COMECO */
+      printf("Em qual comeco de lista deseja remover");
       for (size_t i = 0; i < qnt_lista; i++)
-        printf(" %ld", i);
+        printf(" %ld", i + 1);
       printf(": ");
       scanf("%d", &tmp);
-      printf("Insira o numero que deseja remover: ");
-      setbuf(stdin, NULL);
-      scanf("%c", &letra);
-      remove_ord(&lst[tmp], letra);
-
-      sleep(2);
+      tmp--;
+      if (unshift(lst[tmp], elemento))
+        printf("%f foi removido\n", elemento);
+      else
+        printf("Erro ao remover na posicao %d\n", tmp2);
+      sleep(1);
       system("clear"); //MUDAR PRA CLS PRA SISTEMAS INFERIORES
       break;
     case 4:
-      /* REMOVENDO POSIÇÃO */
-      printf("Em qual lista deseja remover");
+      if (qnt_lista == 0)
+        goto get_back_here;
+      /* INSERE NO FINAL */
+      printf("Em qual final de lista deseja inserir");
       for (size_t i = 0; i < qnt_lista; i++)
-        printf(" %ld", i);
+        printf(" %ld", i + 1);
       printf(": ");
       scanf("%d", &tmp);
-
-      printf("Insira a posicao que deseja remover: ");
-      scanf("%d", &tmp2);
-      if (remove_pos(&lst[tmp], tmp2, &letra))
-        printf("%c foi removido\n", letra);
+      printf("Insira o num que deseja inserir na lista %d: ", tmp);
+      setbuf(stdin, NULL);
+      scanf("%f", &elemento);
+      setbuf(stdin, NULL);
+      tmp--;
+      if (push(lst[tmp], elemento))
+      {
+        printf("Sucesso\n");
+        sleep(0.3);
+        system("clear"); //MUDAR PRA CLS PRA SISTEMAS INFERIORES
+      }
       else
-        printf("Erro ao remover na posicao %d\n", tmp2);
-      sleep(2);
-      system("clear"); //MUDAR PRA CLS PRA SISTEMAS INFERIORES
+      {
+        printf("Erro\n");
+        sleep(1);
+        system("clear"); //MUDAR PRA CLS PRA SISTEMAS INFERIORES
+      }
       break;
     case 5:
-      /* IMPRIMINDO LISTA */
-      printf("Qual lista deseja imprimir");
+      /* REMOVE NO FINAL */
+      if (qnt_lista == 0)
+        goto get_back_here;
+      printf("Em qual final de lista deseja remover");
       for (size_t i = 0; i < qnt_lista; i++)
-        printf(" %ld", i);
+        printf(" %ld", i + 1);
       printf(": ");
       scanf("%d", &tmp);
-      size_t i = 0;
-      while (obtem_valor_elem(&(lst[tmp]), i, &letra))
-      {
-        printf("%c ", letra);
-        i++;
-      }
-      printf("\n");
-      sleep(2);
+      setbuf(stdin, NULL);
+      tmp--;
+      if (pop(lst[tmp], &elemento))
+        printf("%.2f foi removido\n", elemento);
+      else
+        printf("Erro ao remover\n");
+      sleep(1);
       system("clear"); //MUDAR PRA CLS PRA SISTEMAS INFERIORES
       break;
 
     case 6:
-      printf("Qual lista deseja limpar");
+      /* IMPRIMINDO LISTA */
+      if (qnt_lista == 0)
+        goto get_back_here;
+      printf("Qual lista deseja imprimir");
+      for (size_t i = 0; i < qnt_lista; i++)
+        printf(" %ld", i + 1);
+      printf(": ");
+      scanf("%d", &tmp);
+      tmp--;
+      //alocando um array pra guardar os elementos q retirar
+      Deque deque_auxiliar = cria_deque();
+      while (!deque_vazio(lst[tmp]))
+      {
+        shift(lst[tmp], &elemento);
+        push(deque_auxiliar, elemento);
+        printf("[%.2f] ", elemento);
+      }
+      while (!deque_vazio(deque_auxiliar))
+      {
+        shift(deque_auxiliar, &elemento);
+        push(lst[tmp], elemento);
+      }
+      libera_deque(&deque_auxiliar);
+      printf("\n");
+      sleep(1);
+      system("clear"); //MUDAR PRA CLS PRA SISTEMAS INFERIORES
+      break;
+
+    case 7:
+      /*APAGANDO UMA LISTA*/
+      if (qnt_lista == 0)
+        goto get_back_here;
+      /* APAGANDO UMA LISTA*/
+      printf("Qual lista deseja apagar");
       for (size_t i = 0; i < qnt_lista; i++)
       {
         if (lst_ocupados[i] == 1)
-          printf(" %ld", i);
+          printf(" %ld", i + 1);
       }
       printf(": ");
       scanf("%d", &tmp);
-      esvazia_lista(&lst[tmp]);
+      tmp--;
+      libera_deque(&lst[tmp]);
       lst_ocupados[tmp]--;
-      sleep(2);
+      qnt_lista--;
+      printf("\n");
+      sleep(1);
       system("clear"); //MUDAR PRA CLS PRA SISTEMAS INFERIORES
-      break;
-    case 7:
-      //TAMANHO
-      printf("Qual lista deseja ver o tamanho");
-      for (size_t i = 0; i < qnt_lista; i++)
-        printf(" %ld", i);
-      printf(": ");
-      scanf("%d", &tmp);
-      printf("Tamanho da lista %d: %d", tmp, obtem_tamanho(&lst[tmp]));
-
-      sleep(2);
-      system("clear"); //MUDAR PRA CLS PRA SISTEMAS INFERIORES
-
       break;
     case 8:
-      //VER SE AS LISTAS SAO IGUAIS
-      printf("Quais lista vai comparar");
-      for (size_t i = 0; i < qnt_lista; i++)
-        printf(" %ld", i);
-      printf(": ");
-      scanf("%d %d", &tmp, &tmp2);
-      if (listas_iguais(&lst[tmp], &lst[tmp2]))
-        printf("Listas iguais\n");
-      else
-        printf("Listas diferentes\n");
-      sleep(1);
-      system("clear"); //MUDAR PRA CLS PRA SISTEMAS INFERIORES
-
-      break;
-    case 9:
-      //MAIOR ELEMENTO
-      printf("Qual lista deseja ver o tamanho");
-      for (size_t i = 0; i < qnt_lista; i++)
-        printf(" %ld", i);
-      printf(": ");
-      scanf("%d", &tmp);
-      if (maior_elem(&lst[tmp], &letra))
-        printf("Maior elemento %c\n", letra);
-      else
-        printf("Nao consegui achar o maior elemento\n");
-      sleep(1);
-      system("clear"); //MUDAR PRA CLS PRA SISTEMAS INFERIORES
-      break;
-    case 10:
-      //INTERCALAR
-      printf("Quais lista serao intercaladas");
-      for (size_t i = 0; i < qnt_lista; i++)
-        printf(" %ld", i);
-      printf(": ");
-      scanf("%d %d", &tmp, &tmp2);
-      int tmp3 = 3;
-      printf("Qual lista recebera a lista resultante (sera apagada antes de receber os valores): ");
-      scanf("%d", &tmp3);
-      esvazia_lista(&lst[tmp3]);
-      if (intercalar(&lst[tmp], &lst[tmp2], &lst[tmp3]))
-        printf("Listas intercaladas com sucesso\n");
-      else
-        printf("Erro\n");
-      sleep(1);
-      system("clear"); //MUDAR PRA CLS PRA SISTEMAS INFERIORES
-      break;
-    case 11:
       //SAIR DO LOOP
       continuar = 0;
       break;
